7fa86fdef5f7774adc40de509c6ebe54dd5c9ea6
Victor Azzam <victor123uk@gmail.com>
2019-02-12 00:08:34 +0000

    Improved modularity and command parsing

--- demo.py
+++ demo.py
@@ -1,14 +1,28 @@
-from subprocess import Popen
+import readline
+from subprocess import call
 
 tests = {
     # Basic allow/deny
-    "ftp_basic" : "iptables -{} INPUT -p tcp -j DROP --dport 21",
-    "ssh_basic" : "iptables -{} INPUT -p tcp -j DROP --dport 22",
-    "http" : "iptables -{} INPUT -p tcp -j DROP --dport 80",
-    "https" : "iptables -{} INPUT -p tcp -j DROP --dport 443",
+    "ftp_basic" : ["iptables -A INPUT -p tcp -j DROP --dport 21"],
+    "ssh_basic" : ["iptables -A INPUT -p tcp -j DROP --dport 22"],
+    "http" : ["iptables -A INPUT -p tcp -j DROP --dport 80"],
+    "https" : ["iptables -A INPUT -p tcp -j DROP --dport 443"],
 
     # Custom chains
-    "chain_add" : "",
+    "chain_add" : [
+		"echo 123 > /dev/null",
+		"echo 456 > /dev/null"
+	],
+}
+
+destroy = {
+    "ftp_basic" : ["iptables -D INPUT -p tcp -j DROP --dport 21"],
+    "ssh_basic" : ["iptables -D INPUT -p tcp -j DROP --dport 22"],
+    "http" : ["iptables -D INPUT -p tcp -j DROP --dport 80"],
+    "https" : ["iptables -D INPUT -p tcp -j DROP --dport 443"],
+	"chain_add" : [
+		"echo 123 > /dev/null"
+	]
 }
 
 enabled = set()
@@ -18,15 +32,18 @@ try:
 	while 1:
 		print("\033[1J\033[H", end="") # Clear screen
 		print("Test{0}Command\n----{0}-------\n".format(" " * 12), end="")
-		print("\n".join(f"{n:<16}{green * (n in enabled)}{t.format('A')}\033[m" for n,t in tests.items()))
+		for n, t in tests.items():
+			c = green * (n in enabled)
+			print(f"{n:<16}" + f"\n{'':16}".join(f"{c}{x}\033[m" for x in t))
 		ui = input("\nTest: ").strip()
 		if ui in tests:
 			if ui in enabled:
 				enabled.remove(ui)
-				action = "D"
+				action = destroy
 			else:
 				enabled.add(ui)
-				action = "A"
-			Popen(tests[ui].format(action).split())
+				action = tests
+			for cmd in action[ui]:
+				call(cmd.split())
 except (KeyboardInterrupt, EOFError):
 	print()
